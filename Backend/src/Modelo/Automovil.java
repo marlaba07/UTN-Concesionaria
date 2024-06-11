6cfa222e6374d362e279c8a7eed6b8387aac0d8f
Mariano Labatut <marianolabatut@gmail.com>
2024-06-11 19:36:29 -0300

    fix

--- Backend/src/Modelo/Automovil.java
+++ Backend/src/Modelo/Automovil.java
@@ -1,14 +1,18 @@
 package Modelo;
-import Model.Enums.TipoAutomovil;
+import Modelo.Enums.TipoAutomovil;
 
 public class Automovil extends Vehiculo{
     private int cantPuertas;
     private TipoAutomovil tipo;
 
-    public Automovil(int id, String marca, String modelo, String color, Integer anio, Double precio, int cantPuertas, Integer stock,TipoAutomovil tipo) {
-        super(id, marca, modelo, color, anio, precio, stock);
+    public Automovil(){
+
+    }
+
+    public Automovil(int id, String marca, String modelo, String color, Integer anio, Double precio, Integer stock, String tipo, int cantPuertas, TipoAutomovil tipo1) {
+        super(id, marca, modelo, color, anio, precio, stock, tipo);
         this.cantPuertas = cantPuertas;
-        this.tipo = tipo;
+        this.tipo = tipo1;
     }
 
     public int getCantPuertas() {
@@ -18,16 +22,4 @@ public class Automovil extends Vehiculo{
     public void setCantPuertas(int cantPuertas) {
         this.cantPuertas = cantPuertas;
     }
-
-    public TipoAutomovil getTipo() { return tipo;}
-
-    public void setTipo(TipoAutomovil tipo) {this.tipo = tipo;}
-
-    @Override
-    public String toString() {
-        return "Auto: \nMarca: " + getMarca() + " - Modelo: " + getModelo() + " (" + getAnio() + ") - Color: " + getColor() + " - Precio: $" + getPrecio() +
-                " - Stock: " + getStock() + " - " + cantPuertas + " puertas";
-    }
-
-
 }
